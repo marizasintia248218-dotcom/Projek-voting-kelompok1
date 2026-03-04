19394cc5e1048ad7af0024a6a0f45649a5359fd7
marizasintia248218-dotcom <marizasintia248218@student.smkn1kandeman.sch.id>
2026-03-04 10:38:25 +0700

    main

--- halaman1.php
+++ halaman1.php
@@ -4,8 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        .container1 {
+            width: 1240px;
+            height: 500px;
+            background: linear-gradient(to bottom, #BACFD7, #304E79);
+        }
+    </style>
 </head>
 <body>
+    <div class = "halaman1" >
     <h1>Haalloooo Duniaaa</h1>
+</div>
 </body>
 </html>
